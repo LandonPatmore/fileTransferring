6ed6caea05eadbbbc96660b4ebd59dc929ce447a
Landon Patmore <landon.patmore@gmail.com>
2019-03-06 17:22:03 -0500

    Each packet now has a costructor
    - each constructor sets the Opcode according to the type of packet
    - added comment about packets at the top of file

--- Packets.go
+++ Packets.go
@@ -2,28 +2,69 @@ package fileTransferring
 
 // TFTP Implementation of packet types
 // RFC-1350
+
+// Each packet includes fields including zero byte values
+// so it is easier to understand what is going on as
+// certain files are chained together to create a packet
+
 type RRQWRQPacket struct {
-	Opcode [2] byte
+	Opcode   [2] byte // 01/02
 	Filename string
-	Zero byte
-	Mode string
-	ZeroTwo byte
+	Zero     byte
+	Mode     string
+	ZeroTwo  byte
 }
 
 type DataPacket struct {
-	Opcode [2] byte
+	Opcode      [2] byte // 03
 	BlockNumber [2] byte
-	Data [512] byte
+	Data        [512] byte
 }
 
 type ACKPacket struct {
-	Opcode [2] byte
+	Opcode      [2] byte //04
 	BlockNumber [2] byte
 }
 
 type ErrorPacket struct {
-	Opcode [2] byte
-	ErrorCode [2] byte
+	Opcode       [2] byte //05
+	ErrorCode    [2] byte
 	ErrorMessage string
-	Zero byte
+	Zero         byte
+}
+
+func CreateRRQWRQPacket(isRRQ bool) *RRQWRQPacket {
+	var z RRQWRQPacket
+
+	if isRRQ {
+		z.Opcode = [2]byte{0, 1}
+	} else {
+		z.Opcode = [2]byte{0, 2}
+	}
+
+	return &z
+}
+
+func CreateDataPacket() *DataPacket {
+	var d DataPacket
+
+	d.Opcode = [2]byte{0, 3}
+
+	return &d
+}
+
+func CreateACKPacket() *ACKPacket {
+	var a ACKPacket
+
+	a.Opcode = [2]byte{0, 4}
+
+	return &a
+}
+
+func CreateErrorPacket() *ErrorPacket {
+	var e ErrorPacket
+
+	e.Opcode = [2]byte{0, 5}
+
+	return &e
 }
