4fada7d661aaf4c939a6d5f00512544ea54dcf03
Landon Patmore <landon.patmore@gmail.com>
2019-03-06 19:32:57 -0500

    Added cmd line argument interpretation and help

--- server/Server.go
+++ server/Server.go
@@ -1,1 +1,20 @@
-package server
+package main
+
+import (
+	"fileTransferring/shared"
+	"fmt"
+	"net"
+)
+
+func main() {
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":8274")
+	shared.ErrorValidation(err)
+	conn, err := net.ListenUDP("udp", ServerAddr)
+	shared.ErrorValidation(err)
+
+	defer conn.Close()
+
+	message := make([]byte, 1024)
+
+	fmt.Println("Server started...")
+}
---
+++ shared/Utils.go
@@ -0,0 +1,50 @@
+package shared
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// Checks if there are any errors panics if there are
+func ErrorValidation(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func InterpretCommandLineArguments(args [] string) (bool, bool, bool) {
+	var ipv6 bool
+	var slidingWindow bool
+	var dropPackets bool
+
+	for _, arg := range args[1:] {
+		switch arg {
+		case "--ipv6":
+			ipv6 = true
+			break
+		case "--sw":
+			slidingWindow = true
+			break
+		case "--dp":
+			dropPackets = true
+			break
+		case "-h":
+			showHelp()
+			os.Exit(0)
+		default:
+			fmt.Println(`The argument: "` + arg + `" is not valid`)
+			os.Exit(1)
+		}
+	}
+
+	return ipv6, slidingWindow, dropPackets
+}
+
+func showHelp()  {
+	fmt.Println("usage: ./fileTransferring [<options>]")
+	fmt.Println()
+	fmt.Printf("\t--ipv6\t\t %s\n", "Specifiy whether packets are IPv4 vs IPv6 UDP datagrams")
+	fmt.Printf("\t--sw\t\t %s\n", "Specify use of TCP-style sliding windows rather than the sequential acks used in TFTP")
+	fmt.Printf("\t--dp\t\t %s\n" ,"Pretend to drop 1%% of packets")
+}
