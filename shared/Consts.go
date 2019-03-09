a7fe2f6794b5fc68572f023705adb9e18f8ba1c6
Landon Patmore <landon.patmore@gmail.com>
2019-03-09 18:35:01 -0500

    clean up on the types of errors

--- shared/Consts.go
+++ shared/Consts.go
@@ -1,14 +1,18 @@
 package shared
 
 // Errors for error packets
+
+var ERROR_0 = []byte{0, 0}
+var ERROR_2 = []byte{0, 2}
+var ERROR_3 = []byte{0, 3}
+var ERROR_4 = []byte{0, 4}
+var ERROR_5 = []byte{0, 5}
+var ERROR_6 = []byte{0, 6}
+
 // ERROR_0 is a user defined error
-const ERROR_1 = "File not found"
-const ERROR_2 = "Access violation"
-const ERROR_3 = "Disk full or allocation exceeded"
-const ERROR_4 = "Illegal TFTP operation"
-const ERROR_5 = "Unknown transfer ID"
-const ERROR_6 = "File already exists"
-const ERROR_7 = "No such user"
+const ERROR_2_MESSAGE = "Access violation"
+const ERROR_3_MESSAGE = "Disk full or allocation exceeded"
+const ERROR_4_MESSAGE = "Illegal TFTP operation"
+const ERROR_6_MESSAGE = "File already exists"
 
 const PORT = ":8247"
-
