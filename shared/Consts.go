0e6d5397b852c906709ef9a12d0ea592cd641e50
Landon Patmore <landon.patmore@gmail.com>
2019-03-11 02:16:31 -0400

    renamed and added more errors

--- shared/Consts.go
+++ shared/Consts.go
@@ -2,17 +2,19 @@ package shared
 
 // Errors for error packets
 
-var ERROR_0 = []byte{0, 0}
-var ERROR_2 = []byte{0, 2}
-var ERROR_3 = []byte{0, 3}
-var ERROR_4 = []byte{0, 4}
-var ERROR_5 = []byte{0, 5}
-var ERROR_6 = []byte{0, 6}
+var Error0 = []byte{0, 0}
+var Error2 = []byte{0, 2}
+var Error3 = []byte{0, 3}
+var Error4 = []byte{0, 4}
+var Error5 = []byte{0, 5}
+var Error6 = []byte{0, 6}
+var Error8 = []byte{0, 8}
 
 // ERROR_0 is a user defined error
-const ERROR_2_MESSAGE = "Access violation"
-const ERROR_3_MESSAGE = "Disk full or allocation exceeded"
-const ERROR_4_MESSAGE = "Illegal TFTP operation"
-const ERROR_6_MESSAGE = "File already exists"
+const Error2Message = "Access violation"
+const Error3Message = "Disk full or allocation exceeded"
+const Error4Message = "Illegal TFTP operation"
+const Error6Message = "File already exists"
+const Error8Message = "Option value not supported"
 
 const PORT = ":8247"
